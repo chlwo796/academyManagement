69bfb99a28568a495bdc2ca6b20e224bdb8a8820
chlwo796 <chlwo796@gmail.com>
2023-02-23 20:29:46 +0900

    최재환 최종

--- projectTeam1/studentManagement/src/student/Management/Data.java
+++ projectTeam1/studentManagement/src/student/Management/Data.java
@@ -45,10 +45,10 @@ public class Data {
 		scoreMap.put(new Student("홍길동", "010-1111-1111", "1반", "국어"), new Score(80));
 		scoreMap.put(new Student("이순신", "010-2222-2222", "2반", "수학"), new Score(70));
 		scoreMap.put(new Student("유관순1", "010-3333-3333", "1반", "영어"), new Score(100));
-		scoreMap.put(new Student("유관순2", "010-4444-4444", "2반", "수학"), new Score(60));
-		scoreMap.put(new Student("유관순3", "010-1111-1111", "1반", "국어"), new Score(70));
+		scoreMap.put(new Student("유관순2", "010-4444-4444", "2반", "수학"), new Score(59));
+		scoreMap.put(new Student("유관순3", "010-1111-1111", "1반", "국어"), new Score(79));
 		scoreMap.put(new Student("유관순4", "010-5555-5555", "1반", "영어"), new Score(90));
-		scoreMap.put(new Student("유관순5", "010-6666-6666", "2반", "국어"), new Score(95));
+		scoreMap.put(new Student("유관순5", "010-6666-6666", "2반", "국어"), new Score(98));
 		scoreMap.put(new Student("유관순6", "010-7777-7777", "1반", "수학"), new Score(87));
 		scoreMap.put(new Student("유관순7", "010-8888-8888", "1반", "수학"), new Score(88));
 		scoreMap.put(new Student("유관순8", "010-0000-0000", "1반", "수학"), new Score(65));
